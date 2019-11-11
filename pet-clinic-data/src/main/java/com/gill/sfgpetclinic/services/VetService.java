f19f9c70765b81719882133d4669f2f60b6f7dca
Rashpal Gill <rsgillit@gmail.com>
2019-11-11 13:42:31 -0600

    added bootstrap for data. closes #14.

--- pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/VetService.java
@@ -2,6 +2,7 @@ package com.gill.sfgpetclinic.services;
 
 import com.gill.sfgpetclinic.model.Vet;
 
+
 public interface VetService  extends CrudService<Vet, Long> {
 
 }
--- pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.gill.sfgpetclinic.services.map;
 
 import com.gill.sfgpetclinic.model.Owner;
-import com.gill.sfgpetclinic.services.CrudService;
+import com.gill.sfgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -33,6 +33,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     }
 
 
-
-
+    @Override
+    public Owner findByLastName(String lastName) {
+            return null;
+    }
 }
