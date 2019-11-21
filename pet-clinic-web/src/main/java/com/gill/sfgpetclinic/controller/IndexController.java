18c74f5a73fdc3e75f01f25ed1fa058320155b9a
rsgillit <rsgillit@gmail.com>
2019-11-21 16:17:25 -0600

    menu links are broken. Closes #45

--- pet-clinic-web/src/main/java/com/gill/sfgpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/com/gill/sfgpetclinic/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.gill.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,5 +11,11 @@ public class IndexController {
     public String index(){
 
         return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping({"/oups"})
+    public String oupsHandler(){
+        return "not done yet";
     }
 }
