cb9692153a4ba1fff7c7d9f40826c93fd9212806
rsgillit <rsgillit@gmail.com>
2019-11-21 15:37:14 -0600

    Enhance Owners with contact info and Pets on Startup. Closes #33

--- pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,25 @@
 package com.gill.sfgpetclinic.services.map;
 
 import com.gill.sfgpetclinic.model.Owner;
+import com.gill.sfgpetclinic.model.Pet;
 import com.gill.sfgpetclinic.services.OwnerService;
+import com.gill.sfgpetclinic.services.PetService;
+import com.gill.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +33,29 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        Owner savedOwner = null;
+
+        if(object != null){
+            object.getPets().forEach(pet -> {
+                if(pet.getPetType() != null ){
+                    if(pet.getPetType().getId() == null){
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    }
+                } else {
+                    throw new RuntimeException("Pet type is required");
+                }
+
+                if(pet.getId() == null){
+                    Pet savedPet = petService.save(pet);
+                    pet.setId(savedPet.getId());
+                }
+            });
+
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
