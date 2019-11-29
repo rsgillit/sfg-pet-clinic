29b1a56cdf3d062b7748bef991c868dc1aab3aca
rsgillit <rsgillit@gmail.com>
2019-11-29 13:06:02 -0600

    Update Profiles to use Spring Data JPA Services. Closes #48

--- pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/gill/sfgpetclinic/services/map/VetMapService.java
@@ -5,11 +5,13 @@ import com.gill.sfgpetclinic.model.Speciality;
 import com.gill.sfgpetclinic.model.Vet;
 import com.gill.sfgpetclinic.services.SpecialityService;
 import com.gill.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialityService specialityService;
