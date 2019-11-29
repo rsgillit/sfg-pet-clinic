52af5f54b48871a34b669480b8cf87da8b55e7c0
rsgillit <rsgillit@gmail.com>
2019-11-29 15:03:41 -0600

    Add Project Lombok and Refactor. Closes #51

--- pet-clinic-data/src/main/java/com/gill/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/gill/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,18 @@
 package com.gill.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
@@ -13,11 +20,9 @@ public class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
+    public BaseEntity(Long id) {
+        super();
         this.id = id;
     }
 }
--- pet-clinic-web/src/main/java/com/gill/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/gill/sfgpetclinic/bootstrap/DataLoader.java
@@ -63,6 +63,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("1231231234");
 
+
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedPetDogType);
         mikesPet.setOwner(owner1);
